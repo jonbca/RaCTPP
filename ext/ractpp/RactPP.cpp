44658c5f0c7aa7837f8be3f6bba5d9e2db18eea2
Jonathan Abourbih <jon.abourbih@gmail.com>
2010-12-29 21:36:08 +0000

    Added some new functions, changed allocation to use the rb_define_alloc_func instead of the initializer, and updated to use Data_Get_Struct instead.

--- ext/ractpp/RactPP.cpp
+++ ext/ractpp/RactPP.cpp
@@ -24,21 +24,14 @@ extern "C" {
 #endif
 #include "ruby.h"
 
+#define RACT_RB_BOOL(val) \
+			val ? Qtrue : Qfalse;
+
 /* the Ruby class */
 VALUE cFactPP;
-VALUE cFactReasoner;
 
 void free_reasoner(FaCTReasoner *reasoner) {
 	delete reasoner;
-}
-	
-static VALUE t_init(VALUE self) {
-	FaCTReasoner *reasoner = new FaCTReasoner;
-	VALUE r;
-	
-	r = Data_Wrap_Struct(cFactReasoner, NULL, free_reasoner, reasoner);
-	rb_iv_set(self, "@reasoner", r);
-	return self;
 }
 
 static VALUE t_getVersion(VALUE self) {
@@ -48,30 +41,62 @@ static VALUE t_getVersion(VALUE self) {
 
 static VALUE t_isKBPreprocessed(VALUE self) {
 	FaCTReasoner *reasoner;
-	reasoner = (FaCTReasoner *) DATA_PTR(rb_iv_get(self, "@reasoner"));
-	return reasoner->isKBPreprocessed() ? Qtrue : Qfalse;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	return RACT_RB_BOOL(reasoner->isKBPreprocessed());
 }
 
 static VALUE t_isKBClassified(VALUE self) {
 	FaCTReasoner *reasoner;
-	reasoner = (FaCTReasoner *) DATA_PTR(rb_iv_get(self, "@reasoner"));
-	return reasoner->isKBClassified() ? Qtrue : Qfalse;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	return RACT_RB_BOOL(reasoner->isKBClassified());
 }
 
 static VALUE t_isKBRealised(VALUE self) {
 	FaCTReasoner *reasoner;
-	reasoner = (FaCTReasoner *) DATA_PTR(rb_iv_get(self, "@reasoner"));
-	return reasoner->isKBRealised() ? Qtrue : Qfalse;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	return RACT_RB_BOOL(reasoner->isKBRealised());
+}
+
+static VALUE t_clearKB(VALUE self) {
+	FaCTReasoner *reasoner;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	return RACT_RB_BOOL(reasoner->clearKB());
+}
+
+//TODO raises exception
+static VALUE t_classify(VALUE self) {
+	FaCTReasoner *reasoner;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	
+	return Qnil;
+}
+
+//TODO raises exception
+static VALUE t_realise(VALUE self) {
+	FaCTReasoner *reasoner;
+	Data_Get_Struct(self, FaCTReasoner, reasoner);
+	
+	return Qnil;
+}
+
+static VALUE ractpp_alloc(VALUE klass) {
+	VALUE obj;
+	FaCTReasoner *reasoner = new FaCTReasoner;
+	
+	obj = Data_Wrap_Struct(klass, 0, free_reasoner, reasoner);
+	return obj;
 }
 
 void Init_core() {
 	cFactPP = rb_define_class("RactPP", rb_cObject);
-	cFactReasoner = rb_define_class("FaCTReasoner", rb_cObject);
-	rb_define_method(cFactPP, "initialize", (VALUE (*)(...)) t_init, 0);
+	rb_define_alloc_func(cFactPP, ractpp_alloc);
 	rb_define_method(cFactPP, "get_version", (VALUE (*)(...)) t_getVersion, 0);
 	rb_define_method(cFactPP, "kb_preprocessed?", (VALUE (*)(...)) t_isKBPreprocessed, 0);
 	rb_define_method(cFactPP, "kb_realised?", (VALUE (*)(...)) t_isKBRealised, 0);
 	rb_define_method(cFactPP, "kb_classified?", (VALUE(*)(...)) t_isKBClassified, 0);
+	rb_define_method(cFactPP, "clear_kb!", (VALUE(*)(...)) t_clearKB, 0);
+	rb_define_method(cFactPP, "classify", (VALUE(*)(...)) t_classify, 0);
+	rb_define_method(cFactPP, "realise", (VALUE(*)(...)) t_realise, 0);
 }
 
 #ifdef __cplusplus
