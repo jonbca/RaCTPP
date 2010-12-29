3da6aa9d20e89a3657a46d3916b61f22bf01ac73
Jonathan Abourbih <jon.abourbih@gmail.com>
2010-12-29 23:09:07 +0000

    Added copyright notice to spec file

--- test/ractpp_spec.rb
+++ test/ractpp_spec.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2010 Jonathan Abourbih
+# 
+# This file is part of RactPP, the Ruby-FaCT++ Bridge.
+# 
+# RactPP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# RactPP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with RactPP.  If not, see <http://www.gnu.org/licenses/>.
 require_relative '../lib/ractpp/core'
 
 describe RactPP do
